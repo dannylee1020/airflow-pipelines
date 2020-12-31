e799d8654fd9d2e907c3b84bec32ae63eeeae98a
dannylee1020 <dannylee1020@gmail.com>
2020-12-31 16:27:04 +0900

    committing queries for stack_overflow

---
+++ helpers/sql/tags_data.sql
@@ -0,0 +1,20 @@
+-- get tags data
+with overall_data as(
+select 
+  format_timestamp('%Y-%m-%d', creation_date) as created_date,
+  coalesce(format_timestamp('%Y-%m-%d', last_edit_date), 'unspecified') as edited_date,
+  case
+    when tags = '' then 'unspecified'
+    else tags
+  end as tags
+from `bigquery-public-data.stackoverflow.stackoverflow_posts`
+where format_timestamp('%Y-%m-%d', creation_date) = {{ ds }}
+)
+select
+    created_date,
+    tags,
+    count(*)
+from overall_data
+where tags != 'unspecified' 
+
+
