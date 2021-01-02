546dae0b2a07c64abfcdd4c4115f1e3652f4697b
dannylee1020 <dannylee1020@gmail.com>
2021-01-02 15:00:45 +0900

    helper scripts and sql queries

--- helpers/sql/check_data.sql
+++ helpers/sql/check_data.sql
@@ -1,7 +1,7 @@
 -- check if the data exists in the table
 
 select
-    *
+    count(*) > 0
 from `bigquery-public-data.stackoverflow.stackoverflow_posts`
-where format_timestamp('%Y-%m-%d', creation_date) = {{ ds }} 
+where format_timestamp('%Y-%m-%d', creation_date) = "{{ ds }}"
 limit 1
--- helpers/sql/tags_data.sql
+++ helpers/sql/tags_data.sql
@@ -1,4 +1,5 @@
 -- get tags data
+
 with overall_data as(
 select 
   format_timestamp('%Y-%m-%d', creation_date) as created_date,
@@ -8,13 +9,14 @@ select
     else tags
   end as tags
 from `bigquery-public-data.stackoverflow.stackoverflow_posts`
-where format_timestamp('%Y-%m-%d', creation_date) = {{ ds }}
+where format_timestamp('%Y-%m-%d', creation_date) = "{{ ds }}"
 )
+
 select
     created_date,
     tags,
     count(*)
 from overall_data
 where tags != 'unspecified' 
-
+group by 1,2
 
