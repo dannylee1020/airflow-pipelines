339394f7ac46395e861e8a85cde73b2d19a7126e
dannylee1020 <dannylee1020@gmail.com>
2021-02-11 15:11:14 -0800

    dbt integrated dag

---
+++ dags/with_dbt/baseball_dbt_dag.py
@@ -0,0 +1,59 @@
+import airflow
+from datetime import timedelta, datetime
+from airflow import DAG
+
+from airflow.contrib.operators.bigquery_operator import BigQueryOperator
+from airflow.operators.bash_operator import BashOperator
+
+dag_args = {    
+    'owner':'daniel.lee',
+    'depends_on_past': False,
+    'start_date': datetime(2021, 2, 10),
+    'end_date' : datetime(2021, 2, 15),
+    'retry_delay': timedelta(minutes=3),
+}
+
+with DAG(
+    dag_id = 'baseball_dbt',
+    default_args = dag_args,
+    catchup = False,
+    template_searchpath = ['/Users/dhyungseoklee/Projects/airflow/helpers/sql/baseball'], 
+    schedule_interval = '@hourly'
+) as dag:
+
+    t1 = BigQueryOperator(
+        task_id = 'public_data_to_stg',
+        bigquery_conn_id = 'bigquery_default',
+        sql = 'get_public_data.sql',
+        create_disposition = 'CREATE_IF_NEEDED',
+        write_disposition = 'WRITE_TRUNCATE',
+        use_legacy_sql = False,
+        # time_partitioning = {
+        #     'type' : 'DAY'
+        # },
+        destination_dataset_table = 'airflow-sandbox-296122:airflow_dbt.games_post_wide'
+    ) 
+
+
+    t2 = BashOperator(
+        task_id = 'dbt_docs',
+        bash_command = 'cd /Users/dhyungseoklee/Projects/dbt/baseball_dbt && dbt docs generate'
+    )
+
+    t3 = BashOperator(
+        task_id = 'dbt_test',
+        bash_command = 'cd /Users/dhyungseoklee/Projects/dbt/baseball_dbt && dbt test'
+    )
+
+    t4 = BashOperator(
+        task_id = 'dbt_run',
+        bash_command = 'cd /Users/dhyungseoklee/Projects/dbt/baseball_dbt && dbt run'
+    )
+
+
+
+
+t1 >> t2 >> t3 >> t4
+
+
+
